0fae892f92404e8eb5e4ec77b73e85b0cf98583d
leejiwon6315 <leejiwon6315@naver.com>
2023-10-30 16:26:19 +0900

    remove: env 삭제

--- schedule.js
+++ schedule.js
@@ -1,16 +1,24 @@
 import { WebClient } from '@slack/web-api';
 import dotenv from 'dotenv';
-import { executeTmpCoffeeBot } from './tmp.js';
+import schedule from 'node-schedule';
+// import { executeTmpCoffeeBot } from './tmp.js';
 // import { executeCoffeeBot } from './coffee.js';
-import { GET_COFFEE } from './message.js';
+// import { GET_COFFEE } from './src/message.js';
 
 dotenv.config();
 
 const TOKEN = process.env.API_TOKEN;
 const client = new WebClient(TOKEN);
-// const CHANNEL_ID = '';
+const CHANNEL_ID = process.env.TEST_CHANNEL;
+const rule = {
+  hour: 16,
+  minute: 16,
+  dayOfWeek: 1,
+};
 
-let count = false;
+const job = schedule.scheduleJob(rule, () => {
+  requestMessage('스케쥴러 테스트');
+});
 
 const requestMessage = async (message) => {
   try {
@@ -26,7 +34,5 @@ const requestMessage = async (message) => {
 };
 
 (async () => {
-  await executeTmpCoffeeBot(GET_COFFEE, requestMessage, count);
-
-  count = true;
+  await job;
 })();
